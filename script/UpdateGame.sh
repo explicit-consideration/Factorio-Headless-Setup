1be198ca99d109299413c800d450ae8148fb0315
explicit-consideration <code.pub@proton.me>
2025-01-31 14:33:47 +0100

    Add script update

---
+++ script/UpdateGame.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo 'Stop Service'
+sudo systemctl stop factorio.service
+
+echo 'Download Update'
+sudo -u factorio wget 'https://factorio.com/get-download/stable/headless/linux64' -O /tmp/factorio-headless.tar.xz
+
+echo 'Backup Files'
+sudo mkdir /opt/BackupFactorioUpdate
+sudo cp -r /opt/factorio/ /opt/BackupFactorio/
+
+echo 'Install Update'
+sudo -u factorio tar -xf /tmp/factorio-headless.tar.xz -C /opt/
+
+echo 'Restart'
+sudo systemctl daemon-reload
+sudo systemctl restart factorio.service
